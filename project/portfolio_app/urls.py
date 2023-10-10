2b9dac451aaf8bfabf3512661a9ffffb7d0e9db7
Gabriel Ainza <139720159+gabrielainza@users.noreply.github.com>
2023-10-10 08:11:40 -0700

    cambiamos el views a otro directorio.

--- project/portfolio_app/urls.py
+++ project/portfolio_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('iris/', views.iris, name='iris'),
     path('casa/', views.casa, name='casa'),
-    path('descargar_cv/', views.descargar_cv, name='descargar_cv'),
+    path('download_my_pdf', views.download_pdf, name="download_pdf"),
 ]
